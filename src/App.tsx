f640efa01b409a7285762d48ff4958c20a71d4e6
Carlos Vergikosk <carlos@ketl.ch>
2023-06-27 11:49:58 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import PDFViewer from "./components/PDFViewer";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        destroyOnClose
+      >
+        <PDFViewer index={1} />
+      </Modal>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/PDFViewer.tsx
@@ -0,0 +1,112 @@
+import WebViewer, { getInstance } from "@pdftron/pdfjs-express-viewer";
+import React from "react";
+
+const PDFS = [
+  "/src/pdfs/A.pdf",
+  "/src/pdfs/B.pdf",
+  "/src/pdfs/C.pdf",
+  "/src/pdfs/D.pdf",
+  "/src/pdfs/E.pdf",
+  "/src/pdfs/F.pdf",
+  "/src/pdfs/G.pdf",
+  "/src/pdfs/H.pdf",
+];
+
+export type LocRef = {
+  ref: React.RefObject<HTMLDivElement>;
+  index: string;
+  data: {
+    x: number;
+    y: number;
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+    right: number;
+    bottom: number;
+    zIndex: number;
+  };
+};
+
+export const WebViewerContext = React.createContext<{
+  instance?: WebViewerInstance;
+  setInstance?: (instance: WebViewerInstance) => void;
+  locRefs?: LocRef[];
+  setLocRefs?: React.Dispatch<React.SetStateAction<LocRef[]>>;
+}>({});
+
+interface Props {
+  index: string;
+}
+
+export default function PDFViewer({ index }: Props): JSX.Element {
+  const viewer = React.useRef(null);
+  const [instance, setInstance] = React.useState(null);
+
+  const currentViewer = viewer.current;
+
+  console.log({ index });
+
+  const setupInstance = React.useCallback(async () => {
+    WebViewer(
+      {
+        path: "webviewer/A",
+        initialDoc: PDFS[index],
+        disableLogs: true,
+      },
+      viewer.current
+    ).then((instance: WebViewerInstance) => {
+      setInstance(instance);
+
+      const { documentViewer } = instance.Core;
+
+      documentViewer.addEventListener("documentLoaded", () => {
+        instance.UI.setFitMode(instance.UI.FitMode.FitWidth);
+      });
+    });
+  }, [index]);
+
+  const updateInstance = React.useCallback(async () => {
+    instance.UI.loadDocument(PDFS[index], {
+      documentId: index,
+    });
+  }, [index, instance]);
+
+  React.useEffect(() => {
+    return () => {
+      if (!instance) {
+        return;
+      }
+
+      // Ideally it would be closed on unmount, but we cant await things here.
+      // instance.UI.closeDocument()
+      // instance.UI.dispose()
+      // instance.Core.documentViewer.dispose()
+    };
+  }, [instance, currentViewer]);
+
+  const handleindexChange = React.useCallback(async () => {
+    const instance = getInstance();
+
+    console.log({ instance });
+
+    if (instance) {
+      instance.UI.closeDocument();
+      await instance.UI.dispose();
+      await instance.Core.documentViewer.dispose();
+    }
+
+    setupInstance();
+  }, [setupInstance]);
+
+  React.useEffect(() => {
+    handleindexChange();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [index]);
+
+  return React.useMemo(() => {
+    return (
+      <div className="webviewer" style={{ height: "100%" }} ref={viewer}></div>
+    );
+  }, []);
+}
